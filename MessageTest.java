2aec1ef0b123684da71593db47bbc33090ec1ec5
Raazieyah <ST10465704@Rcconnect.edu.za>
2026-05-25 18:23:49 +0200

    Message unit test Part 2

---
+++ MessageTest.java
@@ -0,0 +1,90 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit4TestClass.java to edit this template
+ */
+package com.mycompany.mychatapp;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author david
+ */
+public class MessageTest {
+    
+    public MessageTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of SentMessage method, of class Message.
+     */
+    @Test
+    public void testSentMessage() {
+        System.out.println("SentMessage");
+        Message instance = new Message();
+        String expResult = "";
+        String result = instance.SentMessage();
+        assertEquals(expResult, result);
+        
+    }
+
+    /**
+     * Test of printMessage method, of class Message.
+     */
+    @Test
+    public void testPrintMessage() {
+        System.out.println("printMessage");
+        Message instance = new Message();
+        String expResult = "";
+        String result = instance.printMessage();
+        assertEquals(expResult, result);
+        
+    }
+
+    /**
+     * Test of returnTotalMessages method, of class Message.
+     */
+    @Test
+    public void testReturnTotalMessages() {
+        System.out.println("returnTotalMessages");
+        Message instance = new Message();
+        int expResult = 0;
+        int result = instance.returnTotalMessages();
+        assertEquals(expResult, result);
+        
+    }
+
+    /**
+     * Test of storeMessage method, of class Message.
+     */
+    @Test
+    public void testStoreMessage() {
+        System.out.println("storeMessage");
+        .Message.Message msg = null;
+        Message instance = new Message();
+        instance.storeMessage(msg);
+       
+    }
+    
+}
