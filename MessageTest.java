fdb63c0e2d3b7a3b6d955094f4e00d59958e9b5c
Raazieyah <ST10465704@Rcconnect.edu.za>
2026-06-12 16:40:39 +0200

    Add files via upload
    Updated MessageTest for Part 3

--- MessageTest.java
+++ MessageTest.java
@@ -86,5 +86,104 @@ public class MessageTest {
         instance.storeMessage(msg);
        
     }
+
+    /**
+     * Test of checkMessageID method, of class Message.
+     */
+    @Test
+    public void testCheckMessageID() {
+        System.out.println("checkMessageID");
+        Message instance = null;
+        boolean expResult = false;
+        boolean result = instance.checkMessageID();
+        assertEquals(expResult, result);
+        
+    }
+
+    /**
+     * Test of checkRecipientCell method, of class Message.
+     */
+    @Test
+    public void testCheckRecipientCell() {
+        System.out.println("checkRecipientCell");
+        String RecipientCell = "";
+        Message instance = null;
+        int expResult = 0;
+        int result = instance.checkRecipientCell(RecipientCell);
+        assertEquals(expResult, result);
+        
+    }
+
+    /**
+     * Test of addMessage method, of class Message.
+     */
+    @Test
+    public void testAddMessage() {
+        System.out.println("addMessage");
+        Message message = null;
+        Message instance = null;
+        instance.addMessage(message);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of displaySentMessages method, of class Message.
+     */
+    @Test
+    public void testDisplaySentMessages() {
+        System.out.println("displaySentMessages");
+        Message instance = null;
+        instance.displaySentMessages();
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of displayLongestMessage method, of class Message.
+     */
+    @Test
+    public void testDisplayLongestMessage() {
+        System.out.println("displayLongestMessage");
+        Message instance = null;
+        instance.displayLongestMessage();
+        
+    }
+
+    /**
+     * Test of searchByID method, of class Message.
+     */
+    @Test
+    public void testSearchByID() {
+        System.out.println("searchByID");
+        String id = "";
+        Message instance = null;
+        instance.searchByID(id);
+        
+    }
+
+    /**
+     * Test of searchByRecipient method, of class Message.
+     */
+    @Test
+    public void testSearchByRecipient() {
+        System.out.println("searchByRecipient");
+        String recipient = "";
+        Message instance = null;
+        instance.searchByRecipient(recipient);
+        
+    }
+
+    /**
+     * Test of deleteByMessageHash method, of class Message.
+     */
+    @Test
+    public void testDeleteByMessageHash() {
+        System.out.println("deleteByMessageHash");
+        String messageHash = "";
+        Message instance = null;
+        instance.deleteByMessageHash(messageHash);
+        
+    }
     
 }
